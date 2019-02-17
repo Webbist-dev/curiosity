d76cfc5aa5d53a23107e44f0953c7e7ee38e241c
Alex Bennett <alex_bennett@hotmail.com>
2019-02-17 23:39:03 +0000

    styling

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,51 @@
 <template>
   <div id="app">
-    <div class="inner">
+    <header class="header container">
+      <p>+Instagram</p>
+      <h3><a href="#">@madebystudiojq</a></h3>
+    </header>
 
+    <div class="container">
+      <div class="inner">
+        <div class="row">
+          <div class="squares"></div>
+          <div class="content horizontal-end">
+            <h3>Nasa's Mars Rover <br> Oppertunity concludes <br> a 15-year mission</h3>
+          </div>
+        </div>
+        <div class="row">
+          <div class="content space-bottom">
+            <h3>-Launch date</h3>
+            <h4>November 26, 2011, <br>15:02:00 UTC</h4>
+          </div>
+          <div class="content verticle-end">
+            <h3>-Operator</h3>
+            <h4>Nasa</h4>
+          </div>
+        </div>
+        <div class="row"></div>
+        <div class="row">
+          <div class="content horizontal-end">
+            <h3>-Distance covered</h3>
+            <h4>19.75 km (12.27mi)</h4>
+          </div>
+        </div>
+        <div class="row">
+          <h1>-Curiosity <br> (Rover)-.</h1>
+        </div>
+        <div class="row">
+          <div class="content space-left">
+            <h3>-Mission type</h3>
+            <h4>Mars rover</h4>
+          </div>
+        </div>
+        <div class="row">
+          <div class="content horizontal-end">
+            <h3>-Mission duration</h3>
+            <h4>668 sols (687 days)</h4>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -13,9 +57,5 @@
 </script>
 
 <style lang="scss" scoped>
-  .inner {
-    height: 1000px;
-    background-image: linear-gradient(#1c1c1a .1em, transparent .1em);
-    background-size: 100% 14px;
-  }
+  
 </style>
